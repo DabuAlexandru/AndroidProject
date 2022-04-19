0f44dd5aaac7c0ebad20b43123860091bf11342d
DabuAlexandru <adabu34@gmail.com>
2022-04-19 20:16:52 +0300

    share images

--- app/src/main/java/com/android/example/androidproject/memories/MemoryEntityAdapter.kt
+++ app/src/main/java/com/android/example/androidproject/memories/MemoryEntityAdapter.kt
@@ -1,11 +1,14 @@
 package com.android.example.androidproject.memories
 
+import android.app.Activity
 import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ShareCompat
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.android.example.androidproject.R
@@ -28,11 +31,19 @@ class MemoryEntityAdapter(var memoriesList: List<MemoryEntity>) : RecyclerView.A
         val memoryImage: ImageView = itemView.findViewById(R.id.memory_image)
         val memoryTimestamp: TextView = itemView.findViewById(R.id.memory_timestamp)
         val memoryDescription: TextView = itemView.findViewById(R.id.memory_description)
+        val shareMemoryButton: Button = itemView.findViewById(R.id.share_memory)
 
         fun bind(item: MemoryEntity) {
             memoryImage.setImageURI(item.imageURI.toUri())
             memoryDescription.text = item.description
             memoryTimestamp.text = DateFormat.format("dd-MM-yyyy HH:mm", item.timestamp).toString()
+            shareMemoryButton.setOnClickListener {
+                ShareCompat.IntentBuilder.from(shareMemoryButton.context as Activity)
+                    .setStream(item.imageURI.toUri())
+                    .setType("image/jpeg")
+                    .setChooserTitle("Share your memory")
+                    .startChooser();
+            }
         }
 
         companion object {
